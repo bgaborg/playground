e651dd14a6f4146f668c76c41411b6e29ce49a4b
bgaborg <gab.bota@gmail.com>
2023-03-27 00:19:39 +0200

    progress

--- neetcode/src/main/java/org/gabota/neetcode/slidingwindow/e01BuyOrSellStock.java
+++ neetcode/src/main/java/org/gabota/neetcode/slidingwindow/e01BuyOrSellStock.java
@@ -2,7 +2,22 @@ package org.gabota.neetcode.slidingwindow;
 
 public class e01BuyOrSellStock {
 
+    public static int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxprofit = 0;
+        while (right < prices.length) {
+            if (prices[right] > prices[left]) {
+                maxprofit = Math.max(maxprofit, prices[right] - prices[left]);
+            } else {
+                left=right;
+            }
+            right++;
+        }
+        return maxprofit;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println(maxProfit(new int[] {7,1,5,3,6,4}));
     }
 }
